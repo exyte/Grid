68246211983d749bd102b0a15cfc9a9319e20edf
Denis Obukhov <denis.obukhov@exyte.com>
2020-04-16 20:12:44 +0700

    Change LayoutArrangement description

--- GridLayout/Extensions/LayoutArrangement+description.swift
+++ GridLayout/Extensions/LayoutArrangement+description.swift
@@ -10,26 +10,24 @@ import Foundation
 
 extension LayoutArrangement: CustomStringConvertible {
     var description: String {
-        var iterations = 0
         var result = ""
         let maxRow = self.items.map(\.endPosition.row).max(by: <) ?? 0
         
-        var items = self.items.map { (arrangement: $0, square: $0.area) }
+        var items = self.items.map { (arrangement: $0, area: $0.area) }
 
         for row in 0...maxRow {
             columnsCycle: for column in 0..<self.columnsCount {
                 for (index, item) in items.enumerated() {
-                    iterations += 1
                     if item.arrangement.contains(GridPosition(row: row, column: column)) {
-                        result += item.arrangement.gridItem.tag ?? "-"
-                        items[index].square -= 1
-                        if items[index].square == 0 {
+                        result += item.arrangement.gridItem.tag ?? "."
+                        items[index].area -= 1
+                        if items[index].area == 0 {
                             items.remove(at: index)
                         }
                         continue columnsCycle
                     }
                 }
-                result += "."
+                result += " "
             }
             result += "\n"
         }

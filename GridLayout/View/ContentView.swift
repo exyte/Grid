210a26ddb713ab34f5af0227417e80f7b0c310b3
Denis Obukhov <denis.obukhov@exyte.com>
2020-04-20 22:55:06 +0700

    Set initial view to grid view

--- GridLayout/View/ContentView.swift
+++ GridLayout/View/ContentView.swift
@@ -8,88 +8,14 @@
 
 import SwiftUI
 
-func randomColor(index: Int? = nil) -> Color {
-    if let index = index {
-        srand48(index)
-    }
-    let red = CGFloat(drand48())
-    let green = CGFloat(drand48())
-    let blue = CGFloat(drand48())
-    let color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
-    return Color(color)
-}
-
 struct ContentView: View {
 
-    let rectWidth: CGFloat = 50
-    let rectHeight: CGFloat = 70
-    let spacing: CGFloat = 0
-    @State var gaps: CGFloat = 15
-    
-    let count = 4
-    lazy var totalWidth: CGFloat = CGFloat(count) * (rectWidth + gaps) + 100
-    lazy var totalHeight: CGFloat = CGFloat(count) * (rectHeight + gaps)
-    
-    func rectangle(rowSpan: Int = 1, columnSpan: Int = 1) -> some View {
-        VStack {
-            ZStack(alignment: .center) {
-                Rectangle()
-                    .foregroundColor(randomColor(index: rowSpan + columnSpan * 10))
-                    .frame(width: CGFloat(rowSpan) * (rectWidth) + (CGFloat(rowSpan) - 1) * gaps,
-                           height: CGFloat(columnSpan) * (rectHeight) + (CGFloat(columnSpan) - 1) * gaps,
-                           alignment: .topLeading)
-                    .padding(gaps / 2)
-            }
-        }
-    }
-    
     var body: some View {
-        /*
-         
-         a b c d
-         e e c f
-         e e g g
-         h h h j
-         
-         */
-
-        return VStack {
-            GeometryReader { _ in
-                VStack(spacing: self.spacing) {
-                    HStack(spacing: self.spacing) {
-                        VStack(spacing: self.spacing) {
-                            HStack(spacing: self.spacing) {
-                                self.rectangle()
-                                self.rectangle()
-                            }
-                            self.rectangle(rowSpan: 2, columnSpan: 2)
-                        }
-                        
-                        VStack(spacing: self.spacing) {
-                            HStack(spacing: self.spacing) {
-                                self.rectangle(columnSpan: 2)
-                                VStack(spacing: self.spacing) {
-                                    self.rectangle()
-                                    self.rectangle()
-                                }
-                            }
-                            
-                            self.rectangle(rowSpan: 2)
-                        }
-
-                    }
-                    HStack(spacing: self.spacing) {
-                        self.rectangle(rowSpan: 3)
-
-                        self.rectangle()
-                    }
-                }
-                .background(Color.black.opacity(0.3))
-            }
-            
-            Slider(value: $gaps, in: 0...50)
+        Grid(columnsCount: 4) {
+            Color(.blue)
+            Color(.red).gridSpan(column: 1, row: 3)
+            Color(.green).gridSpan(column: 3, row: 3)
         }
-
     }
 }
 

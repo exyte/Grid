7b41f108c31c13e112bdcaa114ff8ffc67bb4f08
Denis Obukhov <denis.obukhov@exyte.com>
2020-05-23 23:48:31 +0700

    Fix spacing warning

--- Example/Example/Color+hex.swift
+++ Example/Example/Color+hex.swift
@@ -31,7 +31,7 @@ extension Color {
             .sRGB,
             red: Double(r) / 255,
             green: Double(g) / 255,
-            blue:  Double(b) / 255,
+            blue: Double(b) / 255,
             opacity: Double(a) / 255
         )
     }

974f51247397986e35e99815d44abac4040f3eac
Denis Obukhov <denis.obukhov@exyte.com>
2020-05-28 01:16:25 +0700

    Add packing example

---
+++ Example/Example/PackingExample.swift
@@ -0,0 +1,53 @@
+//
+//  PackingExample.swift
+//  Grid_Example
+//
+//  Created by Denis Obukhov on 27.05.2020.
+//  Copyright © 2020 Exyte. All rights reserved.
+//
+
+import SwiftUI
+import ExyteGrid
+
+struct PackingExample: View {
+    
+    @State var gridPacking = GridPacking.sparse
+    
+    private var packingPicker: some View {
+        Picker("Packing", selection: $gridPacking) {
+            withAnimation {
+                ForEach([GridPacking.sparse, GridPacking.dense], id: \.self) {
+                            Text($0 == .sparse ? "SPARSE" : "DENSE")
+                                .tag($0)
+                        }
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+    }
+
+    var body: some View {
+        VStack {
+            self.packingPicker
+  
+            Grid(tracks: 4) {
+                ColorView(.red)
+                
+                ColorView(.black)
+                    .gridSpan(column: 4)
+                
+                ColorView(.purple)
+      
+                ColorView(.orange)
+                ColorView(.green)
+            }
+            .gridPacking(self.gridPacking)
+            .gridAnimation(.default)
+        }
+    }
+}
+
+struct SpacingExample_Previews: PreviewProvider {
+    static var previews: some View {
+        PackingExample()
+    }
+}

af63c1501f66ca51031e9fffc5cd25cc7078ff09
Denis Obukhov <denis.obukhov@exyte.com>
2020-08-12 00:33:29 +0700

    Update Calculator example

--- Example/Example/Calculator.swift
+++ Example/Example/Calculator.swift
@@ -54,18 +54,24 @@ struct Calculator: View {
                     
                     Grid(tracks: self.tracks, spacing: 10) {
                         self.mainArithmeticButtons
-                        self.topButtons
-                        self.clearButton
 
-                        GridGroup {
-                            GridGroup(self.digits, id: \.self) {
-                                //Only homogeneous "if" clauses are supported
-                                if self.mode == .system && $0 == .digit(0) {
-                                      CalcButton($0)
-                                          .gridSpan(column: 2)
-                                  } else {
-                                      CalcButton($0)
-                                  }
+                        if self.mode == .system {
+                            CalcButton(.percent)
+                                .animation(nil)
+                            CalcButton(.sign)
+                                .animation(nil)
+
+                            GridGroup([MathOperation.clear], id: \.self) {
+                                CalcButton($0)
+                            }
+                        }
+
+                        GridGroup(self.digits, id: \.self) {
+                            if self.mode == .system && $0 == .digit(0) {
+                                CalcButton($0)
+                                    .gridSpan(column: 2)
+                            } else {
+                                CalcButton($0)
                             }
                         }
                         
@@ -89,7 +95,7 @@ struct Calculator: View {
         self.isPortrait ? (self.mode == .system ? 4 : 5) : (self.mode == .system ? 10 : 11)
     }
     
-    var landscapeButtons: GridGroup {
+    var landscapeButtons: some View {
         let funcCount = (self.mode == .system ? 30 : 24)
         let functions: [MathOperation] = self.isPortrait ? [] : (0..<funcCount).map { .function($0) }
         return GridGroup(functions, id: \.self) {
@@ -98,38 +104,38 @@ struct Calculator: View {
                 .animation(nil)
         }
     }
-    
-    var mainArithmeticButtons: GridGroup {
+
+    let mainArithmeticOperations: [MathOperation] = [.divide, .multiply, .substract, .add, .equal]
+
+    @GridBuilder
+    var mainArithmeticButtons: some View {
         switch self.mode {
         case .system:
-            let operations: [MathOperation] = [.divide, .multiply, .substract, .add, .equal]
-            return GridGroup(0..<operations.count) {
-                CalcButton(operations[$0])
+            GridGroup(0..<mainArithmeticOperations.count) {
+                CalcButton(mainArithmeticOperations[$0])
                     .gridStart(column: self.tracks.count - 1)
             }
         case .second:
-            return GridGroup {
-                GridGroup([MathOperation.clear], id: \.self) {
-                    CalcButton($0)
-                        .gridStart(column: self.tracks.count - 1)
-                }
-                CalcButton(.divide)
+            GridGroup([MathOperation.clear], id: \.self) {
+                CalcButton($0)
                     .gridStart(column: self.tracks.count - 1)
-                
-                CalcButton(.multiply)
-                    .gridStart(column: self.tracks.count - 1)
-                
-                CalcButton(.substract)
-                    .gridStart(column: self.tracks.count - 2)
-                
-                CalcButton(.add)
-                    .gridStart(column: self.tracks.count - 2)
-                    .gridSpan(row: 2)
-                
-                CalcButton(.equal)
-                    .gridStart(column: self.tracks.count - 3)
-                    .gridSpan(column: 3)
             }
+            CalcButton(.divide)
+                .gridStart(column: self.tracks.count - 1)
+
+            CalcButton(.multiply)
+                .gridStart(column: self.tracks.count - 1)
+
+            CalcButton(.substract)
+                .gridStart(column: self.tracks.count - 2)
+
+            CalcButton(.add)
+                .gridStart(column: self.tracks.count - 2)
+                .gridSpan(row: 2)
+
+            CalcButton(.equal)
+                .gridStart(column: self.tracks.count - 3)
+                .gridSpan(column: 3)
         }
     }
 
@@ -144,30 +150,6 @@ struct Calculator: View {
                       height: height)
     }
 
-    var topButtons: GridGroup {
-        switch self.mode {
-        case .system:
-            return GridGroup {
-                CalcButton(.percent)
-                    .animation(nil)
-                CalcButton(.sign)
-                    .animation(nil)
-            }
-        case .second:
-            return GridGroup.empty
-        }
-    }
-
-    var clearButton: GridGroup {
-        if self.mode == .system {
-            return GridGroup([MathOperation.clear], id: \.self) {
-                CalcButton($0)
-            }
-        } else {
-            return GridGroup.empty
-        }
-    }
-    
     private var modesPicker: some View {
         Picker("Mode", selection: $mode) {
             ForEach([Mode.system, Mode.second], id: \.self) {
